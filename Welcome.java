0982c9970df6393cd22b1c599bc59b716bfe9bfc
lalitarora1989 <lalitarora1989@gmail.com>
2022-11-17 12:24:20 +0530

    day 4

---
+++ Welcome.java
@@ -0,0 +1,13 @@
+
+
+/**
+ * Hello world!
+ *
+ */
+public class Welcome 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome World! From Jenkins" );
+    }
+}
